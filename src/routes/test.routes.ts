f7c0df5d77eedcaaecb2ac7eeab66363a9c5fbb5
Leandro Alfonso <leandroalfonsoporley@gmail.com>
2023-06-28 16:11:31 -0300

    Adds new route to test jwt validation.

--- src/routes/test.routes.ts
+++ src/routes/test.routes.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import { test } from '../controllers/test.controller';
+import jwtValidator from '../middlewares/jwt-validator';
 
 const router = Router();
 
 router.get('/', test);
+router.get('/jwt', [jwtValidator], test);
 
 export default router;
